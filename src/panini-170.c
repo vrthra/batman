29b60e4f1053c1a2c0e8a602f8128b21ba492350
alistair <alistairvu@gmail.com>
2026-05-18 13:23:30 +1000

    Add panini example

---
+++ src/panini-170.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int f170(const char *s) {
+  if (strchr(s, 'a') == NULL) {
+    return 0;
+  }
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
+  char my_string[10240] = {0};
+  if (argc == 1) {
+    read(fileno(stdin), my_string, 10240);
+  } else {
+    int fd = open(argv[1], O_RDONLY);
+    read(fd, my_string, 10240);
+    close(fd);
+  }
+  int res = f170(my_string);
+  return res;
+}
